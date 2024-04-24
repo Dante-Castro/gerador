238db8c424f72a3838d3c58276f15e3335bde694
Dante-Castro <dantecastro111@gmail.com>
2024-04-24 16:53:25 -0400

    criação tela de password

---
+++ src/pages/passwords/componets/passwordItem.js
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React from 'react'
+
+export default function PasswordItem() {
+  return (
+    <View>
+      <Text>passwordItem</Text>
+    </View>
+  )
+}
--- src/pages/passwords/index.js
+++ src/pages/passwords/index.js
@@ -1,15 +1,61 @@
+import { useState, useEffect} from 'react'
+import { View, Text, StyleSheet, FlatList} from 'react-native'
+import {SafeAreaView} from 'react-native-safe-area-context'
+import { useIsFocused} from '@react-navigation/native'
+import useStorage from '../../hoocks/useStorage'
 
-import { View, Text, StyleSheet} from 'react-native'
 
-export function Password(){
+export function Password(){  
+  const [listPassword, setListPassword] = useState([])
+  const focused = useIsFocused();
+  const {getItem} = useStorage();
+
+  useEffect(() => {
+    async function loadPassword(){
+      const passwords = await getItem("@pass")
+      setListPassword(passwords);
+    }
+
+    loadPassword();
+  }, [focused])
+
   return(
-    <View>
-      <Text>Minhas senhas</Text>
-    </View>
+    <SafeAreaView style={{ flex:1,}}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Minhas senhas</Text>
+      </View>
+
+      <View style={styles.content}>
+        <FlatList
+          style= {{flex: 1, paddingTop: 14,}}
+          data={listPassword}
+          keyExtractor={(item) => String(item)}
+          renderItem={({item}) => <Text>{item}</Text>}        
+        />
+
+      </View>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet({
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: "#392DE9",
+    paddingTop: 58,
+    paddingBottom: 14,
+    paddingLeft: 14,
+    paddingRight: 14,
+  },
+  title:{
+    color: "#FFF",
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  content:{
+    flex: 1,
+    paddingLeft: 14,
+    paddingRight: 14,
+  }
 
   
 })
